1d1f247eb6c9afeea01737e86af9e12e676071d7
masunobu <60934434+masunobu@users.noreply.github.com>
2020-10-17 18:41:14 -0400

    Create BinaryTree.java

---
+++ BinaryTree/BinaryTree.java
@@ -0,0 +1,13 @@
+/**
+   Base binary tree class
+*/
+public class BinaryTree
+{
+    protected Node root;
+    
+    // Check if the binary tree is empty.
+    public boolean isEmpty()
+    {
+        return root == null;
+    }    
+}
